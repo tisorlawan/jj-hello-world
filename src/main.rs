0144f270d8abc8faeb2cf7d3fa03dededf3fdb0e
Agung Baptiso Sorlawan <tisorlawan@gmail.com>
2025-06-29 21:13:35 +0700

    refactor printing

--- src/main.rs
+++ src/main.rs
@@ -3,14 +3,11 @@
 /// This is the best implementation of this program to ever exist.
 
 fn main() {
-    print_hello();
-    print_goodbye();
+    print("Hello, world!");
+    print("Goodbye, world!");
 }
 
-fn print_hello() {
-    println!("Hello, world!");
-}
-
-fn print_goodbye() {
-    println!("Goodbye, world!");
+// a function that prints a message
+fn print(m: &str) {
+    println!("{m}")
 }
